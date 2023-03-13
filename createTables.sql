6e1cc1cf23eebff7bc151976350b3d7c9c15fb76
Styyyx <pema.alcantara@gmail.com>
2023-03-13 20:58:02 +0800

    Updates: [Removed] some tables [Add] some tables

--- createTables.sql
+++ createTables.sql
@@ -6,8 +6,8 @@ CREATE TABLE "tb_Hospital" (
 
 CREATE TABLE "tb_Doctor" (
 	"Doctor_ID" VARCHAR(10) NOT NULL PRIMARY KEY,
+	"Hospital_ID" VARCHAR (10) NOT NULL FOREIGN KEY REFERENCES "tb_Hospital"(Hospital_ID),
 	"DoctorName" VARCHAR(75) NOT NULL,
-	"DoctorLocation" VARCHAR(50) NOT NULL,
 	"Specialization" VARCHAR(20) NOT NULL
 );
 
@@ -23,35 +23,27 @@ CREATE TABLE "tb_Deceased" (
 	"CauseofDeath" VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE "tb_Professional" (
+	"Professional_ID" VARCHAR(10) NOT NULL PRIMARY KEY,
+	"ProfessionalName" VARCHAR(75) NOT NULL,
+	"ProfessionalType" VARCHAR(15) NOT NULL
+);
+
 CREATE TABLE "tb_Procedure" (
-	"ProcedureType" VARCHAR(10) NOT NULL PRIMARY KEY,
-	"ProcedureDetails" VARCHAR(50) NOT NULL,
-	"Performer" VARCHAR(50) NOT NULL
+	"Procedure_ID" INT NOT NULL PRIMARY KEY,
+	"ProcedureType" VARCHAR(15) NOT NULL,
+	"ProcedureDetails" VARCHAR(250) NOT NULL
 );
 
 CREATE TABLE "tb_Request" (
 	"Request_ID" VARCHAR(10) NOT NULL PRIMARY KEY,
 	"Deceased_ID" VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES "tb_Deceased"(Deceased_ID),
-	"ProcedureType" VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES "tb_Procedure"(ProcedureType),
+	"Procedure_ID" INT NOT NULL FOREIGN KEY REFERENCES "tb_Procedure"(Procedure_ID),
+	"Professional_ID" VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES "tb_Professional"(Professional_ID),
 	"RequestDate" DATE NOT NULL,
-	"RequestReason" VARCHAR(20) NOT NULL,
+	"RequestReason" VARCHAR(100) NOT NULL,
 	"RequesterName" VARCHAR(75) NOT NULL,
-);
-
-CREATE TABLE "tb_Approval" (
-	"Approval_ID" VARCHAR(10) NOT NULL PRIMARY KEY,
-	"Request_ID" VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES "tb_Request"(Request_ID),
-	"ApprovedPerformer" VARCHAR(50) NOT NULL,	
-);
-
-CREATE TABLE "tb_Pathologist" (
-	"Pathologist_ID" VARCHAR(10) NOT NULL PRIMARY KEY,
-	"PathologistName" VARCHAR(75) NOT NULL,	
-);
-
-CREATE TABLE "tb_Mortician" (
-	"Mortician_ID" VARCHAR(10) NOT NULL PRIMARY KEY,
-	"MorticianName" VARCHAR(75) NOT NULL,	
+	"IsApproved" BIT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE "tb_Mortuary" (
@@ -59,4 +51,11 @@ CREATE TABLE "tb_Mortuary" (
 	"MortuaryName" VARCHAR(75) NOT NULL,
 	"MortuaryLocation" VARCHAR(50) NOT NULL,
 	"Capacity" INT NOT NULL,	
+);
+
+CREATE TABLE "tb_MortuaryLogs" (
+	"Log_ID" VARCHAR(10) NOT NULL PRIMARY KEY,
+	"Mortuary_ID" VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES "tb_Mortuary"(Mortuary_ID),
+	"Deceased_ID" VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES "tb_Deceased"(Deceased_ID),
+	"DateReceived" DATE NOT NULL
 );
